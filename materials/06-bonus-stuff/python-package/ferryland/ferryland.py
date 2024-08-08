3180066e0c1416611b37a3a55b190c8b129b5522
SamEdwardes <sam.edwardes@posit.co>
2024-08-08 12:56:21 -0400

    Working python package

---
+++ materials/06-bonus-stuff/python-package/ferryland/ferryland.py
@@ -0,0 +1,87 @@
+import datetime
+import os
+from dataclasses import dataclass, field
+
+import httpx
+import polars as pl
+
+
+@dataclass
+class VesselsAPI:
+    """
+    API endpoint for VessselsAPI: https://www.wsdot.wa.gov/ferries/api/vessels/rest/help
+    """
+    base_url: str = "https://www.wsdot.wa.gov/Ferries/API/Vessels/rest"
+    wsdot_access_code: str = field(default_factory=lambda: os.environ["WSDOT_ACCESS_CODE"])
+    params: dict = field(init=False)
+
+    def __post_init__(self):
+        self.params = {"apiaccesscode": self.wsdot_access_code}
+
+    def call_api(self, endpoint: str, timeout: float = 10.0) -> pl.DataFrame:
+        with httpx.Client(
+            base_url=self.base_url, params=self.params, timeout=timeout
+        ) as client:
+            r = client.get(endpoint)
+            data = r.json()
+            df = pl.DataFrame(data)
+            return df 
+
+    def cache_flush_date(self) -> pl.DataFrame:
+        endopint = "/cacheflushdate"
+        return self.call_api(endopint)
+
+    def vessel_accommodations(
+        self, vessel_id: str | None = None) -> pl.DataFrame:
+        if vessel_id:
+            endpoint = f"/vesselaccommodations/{vessel_id}"
+        else:
+            endpoint = "/vesselaccommodations"
+        return self.call_api(endpoint)
+
+    def vessel_basics(self, vessel_id: str | None = None) -> pl.DataFrame:
+        if vessel_id:
+            endpoint = f"/vesselbasics/{vessel_id}"
+        else:
+            endpoint = "/vesselbasics"
+        return self.call_api(endpoint)
+
+    def vessel_history(
+        self,
+        vessel_name: str | None = None,
+        date_start: datetime.date | None = None,
+        date_end: datetime.date | None = None,
+    ) -> pl.DataFrame:
+        if vessel_name and date_start and date_end:
+            url = f"{self.base_url}/vesselhistory/{vessel_name}/{date_start}/{date_end}"
+        elif vessel_name or date_start or date_end:
+            raise ValueError(
+                "Must provide all of `vessel_name`, `date_start`, and `date_end` or none of the arguments"
+            )
+        else:
+            url = f"{self.base_url}/vesselhistory"
+        return self.call_api(url, timeout=30.0)
+
+    def vessel_locations(
+        self, vessel_id: str | None = None) -> pl.DataFrame:
+        if vessel_id:
+            endpoint = f"/vessellocations/{vessel_id}"
+        else:
+            endpoint = "/vessellocations"
+        return self.call_api(endpoint)
+
+    def vessel_stats(self, vessel_id: str | None = None) -> pl.DataFrame:
+        if vessel_id:
+            endpoint = f"/vesselstats/{vessel_id}"
+        else:
+            endpoint = "/vesselstats"
+        return self.call_api(endpoint)
+
+    def vessel_verbose(
+        self, vessel_id: str | None = None) -> pl.DataFrame:
+        if vessel_id:
+            endpoint = f"/vesselverbose/{vessel_id}"
+        else:
+            endpoint = "/vesselverbose"
+        return self.call_api(endpoint)
+
---
+++ materials/06-bonus-stuff/python-package/tests/test_vessel_apis.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+from ferryland.ferryland import VesselsAPI
+from rich.console import Console
+
+console = Console()
+vessels_api = VesselsAPI()
+
+console.rule("Vessel Accomodations")
+vessel_accommodations = vessels_api.vessel_accommodations()
+print(vessel_accommodations)
+
+console.rule("Vessel History")
+vessel_history = vessels_api.vessel_history(
+    vessel_name="Spokane",
+    date_start=date(2024, 1, 1),
+    date_end=date(2024, 1, 7)
+)
+print(vessel_history)
