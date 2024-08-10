24b8b1b78710adbf05efeaab70679fc952a549ac
Michael Beigelmacher <michael.beigelmacher@posit.co>
2024-08-10 17:27:57 +0000

    get model working with custom step now

--- materials/03-model-training/api/app.py
+++ materials/03-model-training/api/app.py
@@ -2,20 +2,22 @@ from vetiver import VetiverModel
 from dotenv import load_dotenv, find_dotenv
 import vetiver
 import pins
+from sklearn.base import BaseEstimator, TransformerMixin
+import sys
 
-from sklearn.base import TransformerMixin
-
-class DenseTransformer(TransformerMixin):
-    def fit(self, X, y=None, **fit_params):
+class DenseTransformer(TransformerMixin, BaseEstimator):
+    def fit(self, X, y=None, **params):
         return self
 
-    def transform(self, X, y=None, **fit_params):
+    def transform(self, X, y=None, **params):
         return X.toarray()
+
+setattr(sys.modules['__main__'], 'DenseTransformer', DenseTransformer)
+
 load_dotenv(find_dotenv())
 
 b = pins.board_connect(server_url='https://pub.ferryland.posit.team/', allow_pickle_read=True)
-v = VetiverModel.from_pin(b, 'brooklynbagel/ferry_delay', version = '266')
+v = VetiverModel.from_pin(b, 'brooklynbagel/ferry_delay')
 
 vetiver_api = vetiver.VetiverAPI(v)
 api = vetiver_api.app
-# vetiver_api.run()
